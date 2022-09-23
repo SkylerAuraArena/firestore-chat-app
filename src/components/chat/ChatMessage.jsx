cda6007c8caaee8b61444e2dcbe0eea758500971
SkylerAuraArena <skylerauraarena@gmail.com>
2022-09-23 18:25:54 +0200

    Correction du composant message avec row-reverse plutôt qu'une condition

--- src/components/chat/ChatMessage.jsx
+++ src/components/chat/ChatMessage.jsx
@@ -7,7 +7,7 @@ const ChatMessage = (props) => {
     const dateMsg = props.date ? props.date.toDate().toLocaleString().replace(regex,"/").replace(regex2," à ") : "Récupération depuis le serveur en cours..."
     const uid = props.user ? props.user.uid : null
  
-    const divCss = "flex items-center gap-1 w-full text-white"
+    const divCss = "flex items-center justify-end gap-1 w-full text-white"
     const logoCss = "flex justify-center items-center w-12 h-12 rounded-full"
     const pCss = "leading-6 px-4 py-3 rounded-3xl max-w-full break-words sm:px-3"
     const { senderEmail, senderId } = props.message
@@ -17,31 +17,16 @@ const ChatMessage = (props) => {
     let msgCss = ""
 
     if(uid === senderId){
-        divMsgCss = `${divCss} justify-end`
+        divMsgCss = `${divCss}`
         imgCss = `${logoCss} bg-indigo-400`
         msgCss = `${pCss} bg-indigo-400`
     } else {
-        divMsgCss = `${divCss}`
+        divMsgCss = `${divCss} flex-row-reverse`
         imgCss = `${logoCss} bg-orange-300`
         msgCss = `${pCss} bg-orange-300`
     }
 
     const trigramme = senderEmail.substr(0,3).toUpperCase()
-    const msg = uid === senderId ? (
-        <div className={divMsgCss} onMouseEnter={()=>handleEnter()} onMouseLeave={()=>handleExit()}>
-            <span className={msgCss}>{msgTxt}</span>
-            <div>
-                <p className={imgCss}>{trigramme}</p>
-            </div>
-        </div>
-    ) : (
-        <div className={divMsgCss} onMouseEnter={()=>handleEnter()} onMouseLeave={()=>handleExit()}>
-            <div>
-                <p className={imgCss}>{trigramme}</p>
-            </div>
-            <span className={msgCss}>{msgTxt}</span>
-        </div>
-    )
 
     const handleEnter = () => {
         setMsgTxt(`Date : ${dateMsg}`)
@@ -51,9 +36,12 @@ const ChatMessage = (props) => {
     }
 
     return(
-        <>
-            { msg }
-        </>
+        <div className={divMsgCss} onMouseEnter={()=>handleEnter()} onMouseLeave={()=>handleExit()}>
+            <span className={msgCss}>{msgTxt}</span>
+            <div>
+                <p className={imgCss}>{trigramme}</p>
+            </div>
+        </div>
     )
 }
 
